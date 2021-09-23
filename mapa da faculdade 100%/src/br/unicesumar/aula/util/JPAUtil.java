16e4c22a497d29622fe57e390f472d3bbbaecce3
jccesar22 <60848753+jccesar22@users.noreply.github.com>
2021-09-22 22:53:14 -0300

    Criando a class percitencia

--- mapa da faculdade 100%/src/br/unicesumar/aula/util/JPAUtil.java
+++ mapa da faculdade 100%/src/br/unicesumar/aula/util/JPAUtil.java
@@ -13,4 +13,4 @@ public class JPAUtil {
         }
         return entityManagerFactory.createEntityManager();
     }
-
+}
---
+++ mapa da faculdade 100%/src/javax/persistence/Persistence.java
@@ -0,0 +1,10 @@
+package javax.persistence;
+
+public class Persistence {
+
+	public static EntityManagerFactory createEntityManagerFactory(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
